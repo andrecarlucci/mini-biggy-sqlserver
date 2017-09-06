18120fd47929bc393a9776320917826f5ab1fe04
andrecarlucci <andrecarlucci@gmail.com>
2017-09-06 16:43:25 -0300

    initial

---
+++ Mini-Biggy.Storage.SqlServer.Cmd/Program.cs
@@ -0,0 +1,48 @@
+﻿using MiniBiggy;
+using MiniBiggy.SaveStrategies;
+using MiniBiggy.Serializers;
+using MiniBiggy.Storage.SqlServer;
+using System;
+
+namespace Mini_Biggy.Storage.SqlServer.Cmd {
+    class Program {
+        static void Main(string[] args) {
+            var conn = "Server=tcp:powerhub-sqlserver.database.windows.net,1433;Initial Catalog=powerhub-sql-prod;Persist Security Info=False;User ID=way2;Password=W2Energy#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //var conn = @"Server=CARLUCCI-WAY2\SQLEXPRESS;Database=mini-biggy;Trusted_Connection=True;MultipleActiveResultSets=true";
+            Console.WriteLine("Hello MiniBiggy on SqlServer!");
+
+            var list = Create.ListOf<Tweet>()
+                          .SavingOnSqlServer()
+                          .KeepingLatest(10)
+                          .WithConnectionString(conn)
+                          .SavingOnTable("tweets")
+                          .UsingPrettyJsonSerializer()
+                          .BackgroundSavingEveryTwoSeconds();
+
+            list.Saved += (sender, eventArgs) => {
+                Console.WriteLine("saved");
+            };
+
+
+            Console.WriteLine("Hello, hit enter to create and save a tweet");
+            while (true) {
+                var line = Console.ReadLine();
+                if (line == "exit") {
+                    break;
+                }
+                list.Add(new Tweet {
+                    DateTime = DateTime.Now,
+                    Message = line,
+                    Id = DateTime.Now.Second
+                });
+            }
+            Console.WriteLine("End");
+        }
+
+        public class Tweet {
+            public int Id { get; set; }
+            public string Message { get; set; }
+            public DateTime DateTime { get; set; }
+        }
+    }
+}
---
+++ Mini-Biggy.Storage.SqlServer/SqlServerStorage.cs
@@ -0,0 +1,83 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace MiniBiggy.Storage.SqlServer {
+    public class SqlServerStorage : IDataStore {
+        public string ConnectionString { get; }
+        public string TableFullName { get; set; }
+        public int VersionsToKeep { get; }
+
+        public SqlServerStorage(string connectionString, string tableFullName, int versionsToKeep = 100) {
+            ConnectionString = connectionString;
+            TableFullName = tableFullName;
+            VersionsToKeep = versionsToKeep;
+            EnsureTables();
+        }
+
+        public void EnsureTables() {
+            var commandText = $@"
+                    IF (NOT EXISTS (SELECT * 
+                                FROM INFORMATION_SCHEMA.TABLES 
+                                where  TABLE_NAME = '{TableFullName}'))
+                    BEGIN
+                        CREATE TABLE {TableFullName} (
+	                        version int NOT NULL IDENTITY (1, 1),
+	                        datetime datetime NOT NULL DEFAULT GETDATE(),
+	                        data varbinary(MAX) NOT NULL,
+	                        CONSTRAINT [PK_{TableFullName}] PRIMARY KEY CLUSTERED (version)
+                        )
+                    END
+                ";
+            using (var conn = new SqlConnection(ConnectionString)) {
+                conn.Open();
+                using (var command = new SqlCommand(commandText, conn)) {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public Task<byte[]> ReadAllAsync() {
+            var commandText = $@"
+                select data from {TableFullName}
+                where version = (select max(version) from {TableFullName})
+            ";
+            using (var conn = new SqlConnection(ConnectionString)) {
+                conn.Open();
+                using (var command = new SqlCommand(commandText, conn)) {
+                    return Task.FromResult((byte[])(command.ExecuteScalar() ?? new byte[0]));
+                }
+            }
+        }
+
+        public Task WriteAllAsync(byte[] list) {
+            var commandText = $@"
+                insert into {TableFullName} (data) values (@data)
+            ";
+            using (var conn = new SqlConnection(ConnectionString)) {
+                conn.Open();
+                using (var cmd = new SqlCommand(commandText, conn)) {
+                    cmd.Parameters.Add("@data", SqlDbType.VarBinary).Value = list;
+                    cmd.ExecuteNonQuery();
+                }
+                DeleteExtraEntries(conn);
+            }
+            return Task.CompletedTask;
+        }
+
+        private void DeleteExtraEntries(SqlConnection conn) {
+            var commandText = $@"
+                delete from {TableFullName} where version <= (select top 1 version - {VersionsToKeep} from {TableFullName} order by version desc)
+            ";
+            using (var cmd = new SqlCommand(commandText, conn)) {
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public static class CreateListExtensions {
+        public static IChooseSerializer<T> SavingOnSqlServer<T>(this CreateList<T> createList) where T : new() {
+            return createList;
+        }
+    }
+}
